184926d0fa2d0cd01af28f26ecf546bfcce58188
Theobl08 <theobl08@gmail.com>
2025-05-21 14:44:16 +0200

    Simplified block loot table data generation

--- src/main/java/net/theobl/extension/datagen/ModBlockLootTableProvider.java
+++ src/main/java/net/theobl/extension/datagen/ModBlockLootTableProvider.java
@@ -12,6 +12,7 @@ import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.NetherWartBlock;
+import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
@@ -21,6 +22,8 @@ import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePrope
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.theobl.extension.block.ModBlocks;
 import net.theobl.extension.item.ModItems;
 
@@ -36,95 +39,30 @@ public class ModBlockLootTableProvider extends BlockLootSubProvider {
     protected void generate() {
         HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
 
-        this.dropSelf(ModBlocks.STONE_WALL.get());
-        this.dropSelf(ModBlocks.POLISHED_GRANITE_WALL.get());
-        this.dropSelf(ModBlocks.POLISHED_DIORITE_WALL.get());
-        this.dropSelf(ModBlocks.POLISHED_ANDESITE_WALL.get());
-        this.dropSelf(ModBlocks.PRISMARINE_BRICK_WALL.get());
-        this.dropSelf(ModBlocks.DARK_PRISMARINE_WALL.get());
-        this.dropSelf(ModBlocks.PURPUR_WALL.get());
-        this.dropSelf(ModBlocks.QUARTZ_WALL.get());
-
-        this.dropSelf(ModBlocks.SMOOTH_STONE_STAIRS.get());
-        this.dropSelf(ModBlocks.NETHERITE_STAIRS.get());
-
-        this.dropSelf(ModBlocks.TINTED_GLASS_PANE.get());
-
-        this.dropSelf(ModBlocks.POLISHED_STONE.get());
-        this.dropSelf(ModBlocks.POLISHED_STONE_STAIRS.get());
-        this.add(ModBlocks.POLISHED_STONE_SLAB.get(), block -> createSlabItemTable(ModBlocks.POLISHED_STONE_SLAB.get()));
-        this.dropSelf(ModBlocks.POLISHED_STONE_WALL.get());
-
-        this.dropSelf(ModBlocks.MOSSY_COBBLED_DEEPSLATE.get());
-        this.dropSelf(ModBlocks.MOSSY_COBBLED_DEEPSLATE_STAIRS.get());
-        this.add(ModBlocks.MOSSY_COBBLED_DEEPSLATE_SLAB.get(), block -> createSlabItemTable(ModBlocks.MOSSY_COBBLED_DEEPSLATE_SLAB.get()));
-        this.dropSelf(ModBlocks.MOSSY_COBBLED_DEEPSLATE_WALL.get());
-
-        this.dropSelf(ModBlocks.MOSSY_DEEPSLATE_BRICKS.get());
-        this.dropSelf(ModBlocks.MOSSY_DEEPSLATE_BRICK_STAIRS.get());
-        this.add(ModBlocks.MOSSY_DEEPSLATE_BRICK_SLAB.get(), block -> createSlabItemTable(ModBlocks.MOSSY_DEEPSLATE_BRICK_SLAB.get()));
-        this.dropSelf(ModBlocks.MOSSY_DEEPSLATE_BRICK_WALL.get());
-
-        this.dropSelf(ModBlocks.SMOOTH_BASALT_STAIRS.get());
-        this.add(ModBlocks.SMOOTH_BASALT_SLAB.get(), block -> createSlabItemTable(ModBlocks.SMOOTH_BASALT_SLAB.get()));
-
-        this.dropSelf(ModBlocks.QUARTZ_BRICK_STAIRS.get());
-        this.add(ModBlocks.QUARTZ_BRICK_SLAB.get(), block -> createSlabItemTable(ModBlocks.QUARTZ_BRICK_SLAB.get()));
-        this.dropSelf(ModBlocks.QUARTZ_BRICK_WALL.get());
-
-        this.dropSelf(ModBlocks.NETHER_BRICK_TILES.get());
-        this.dropSelf(ModBlocks.NETHER_BRICK_TILE_STAIRS.get());
-        this.add(ModBlocks.NETHER_BRICK_TILE_SLAB.get(), block -> createSlabItemTable(ModBlocks.NETHER_BRICK_TILE_SLAB.get()));
-        this.dropSelf(ModBlocks.NETHER_BRICK_TILE_WALL.get());
-
-        this.dropSelf(ModBlocks.CRACKED_RED_NETHER_BRICKS.get());
-        this.dropSelf(ModBlocks.RED_NETHER_BRICK_FENCE.get());
-        this.dropSelf(ModBlocks.CHISELED_RED_NETHER_BRICKS.get());
-        this.dropSelf(ModBlocks.RED_NETHER_BRICK_TILES.get());
-        this.dropSelf(ModBlocks.RED_NETHER_BRICK_TILE_STAIRS.get());
-        this.add(ModBlocks.RED_NETHER_BRICK_TILE_SLAB.get(), block -> createSlabItemTable(ModBlocks.RED_NETHER_BRICK_TILE_SLAB.get()));
-        this.dropSelf(ModBlocks.RED_NETHER_BRICK_TILE_WALL.get());
-
-        LootItemCondition.Builder lootItemConditionBuilder = LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.BLUE_NETHER_WART.get())
-                .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(NetherWartBlock.AGE, 3));
-        this.add(ModBlocks.BLUE_NETHER_WART.get(), block ->
-                LootTable.lootTable().withPool(this.applyExplosionDecay(block, LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F))
-                        .add(LootItem.lootTableItem(ModItems.BLUE_NETHER_WART)
-                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(2.0F, 4.0F))
-                                        .when(lootItemConditionBuilder))
-                                .apply(ApplyBonusCount.addUniformBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE))
-                                        .when(lootItemConditionBuilder))))));
-
-        this.dropSelf(ModBlocks.BLUE_NETHER_BRICKS.get());
-        this.dropSelf(ModBlocks.CRACKED_BLUE_NETHER_BRICKS.get());
-        this.dropSelf(ModBlocks.BLUE_NETHER_BRICK_STAIRS.get());
-        this.add(ModBlocks.BLUE_NETHER_BRICK_SLAB.get(), block -> createSlabItemTable(ModBlocks.BLUE_NETHER_BRICK_SLAB.get()));
-        this.dropSelf(ModBlocks.BLUE_NETHER_BRICK_WALL.get());
-        this.dropSelf(ModBlocks.BLUE_NETHER_BRICK_FENCE.get());
-        this.dropSelf(ModBlocks.CHISELED_BLUE_NETHER_BRICKS.get());
-        this.dropSelf(ModBlocks.BLUE_NETHER_BRICK_TILES.get());
-        this.dropSelf(ModBlocks.BLUE_NETHER_BRICK_TILE_STAIRS.get());
-        this.add(ModBlocks.BLUE_NETHER_BRICK_TILE_SLAB.get(), block -> createSlabItemTable(ModBlocks.BLUE_NETHER_BRICK_TILE_SLAB.get()));
-        this.dropSelf(ModBlocks.BLUE_NETHER_BRICK_TILE_WALL.get());
-
-        this.dropSelf(ModBlocks.SOUL_SANDSTONE.get());
-        this.dropSelf(ModBlocks.SOUL_SANDSTONE_STAIRS.get());
-        this.add(ModBlocks.SOUL_SANDSTONE_SLAB.get(), block -> createSlabItemTable(ModBlocks.SOUL_SANDSTONE_SLAB.get()));
-        this.dropSelf(ModBlocks.SOUL_SANDSTONE_WALL.get());
-        this.dropSelf(ModBlocks.CHISELED_SOUL_SANDSTONE.get());
-        this.dropSelf(ModBlocks.SMOOTH_SOUL_SANDSTONE.get());
-        this.dropSelf(ModBlocks.SMOOTH_SOUL_SANDSTONE_STAIRS.get());
-        this.add(ModBlocks.SMOOTH_SOUL_SANDSTONE_SLAB.get(), block -> createSlabItemTable(ModBlocks.CUT_SOUL_SANDSTONE_SLAB.get()));
-        this.dropSelf(ModBlocks.CUT_SOUL_SANDSTONE.get());
-        this.add(ModBlocks.CUT_SOUL_SANDSTONE_SLAB.get(), block -> createSlabItemTable(ModBlocks.SMOOTH_SOUL_SANDSTONE_SLAB.get()));
-
-        this.dropSelf(ModBlocks.SOUL_O_LANTERN.get());
-        this.dropSelf(ModBlocks.REDSTONE_O_LANTERN.get());
-
-        this.dropSelf(ModBlocks.REDSTONE_LANTERN.get());
-        this.add(ModBlocks.REDSTONE_CAMPFIRE.get(),
-                block -> this.createSilkTouchDispatchTable(block, this.applyExplosionCondition(block, LootItem.lootTableItem(Items.REDSTONE)
-                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(2.0F))))));
+        for (DeferredHolder<Block, ? extends Block> block : ModBlocks.BLOCKS.getEntries()) {
+            if(block.get() instanceof SlabBlock) {
+                this.add(block.get(), this::createSlabItemTable);
+            }
+            else if (block.get().defaultBlockState().is(ModBlocks.BLUE_NETHER_WART.get())) {
+                LootItemCondition.Builder lootItemConditionBuilder = LootItemBlockStatePropertyCondition.hasBlockStateProperties(block.get())
+                        .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(NetherWartBlock.AGE, 3));
+                this.add(block.get(),
+                        block1 -> LootTable.lootTable().withPool(this.applyExplosionDecay(block1, LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F))
+                                .add(LootItem.lootTableItem(block.get())
+                                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(2.0F, 4.0F))
+                                                .when(lootItemConditionBuilder))
+                                        .apply(ApplyBonusCount.addUniformBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE))
+                                                .when(lootItemConditionBuilder))))));
+            }
+            else if (block.get().defaultBlockState().is(ModBlocks.REDSTONE_CAMPFIRE.get())) {
+                this.add(block.get(),
+                        block1 -> this.createSilkTouchDispatchTable(block1, this.applyExplosionCondition(block1, LootItem.lootTableItem(Items.REDSTONE)
+                                .apply(SetItemCountFunction.setCount(ConstantValue.exactly(2.0F))))));
+            }
+            else {
+                this.dropSelf(block.get());
+            }
+        }
 
         // Modify some minecraft loot tables
         this.add(Blocks.DIRT_PATH, block -> this.createSingleItemTableWithSilkTouch(block, Blocks.DIRT));
