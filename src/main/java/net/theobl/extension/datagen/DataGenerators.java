ea02d91c7870628bfffb43cc8c087330e5faf21c
Theobl08 <98655331+Theobl08@users.noreply.github.com>
2025-05-28 10:49:34 +0200

    Remove unused classes

--- src/main/java/net/theobl/extension/datagen/DataGenerators.java
+++ src/main/java/net/theobl/extension/datagen/DataGenerators.java
@@ -37,7 +37,5 @@ public class DataGenerators {
         generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModLanguageProvider(packOutput, "en_us"));
-        //generator.addProvider(event.includeClient(), new ModModelProvider(packOutput));
-
     }
 }
