1a57c445edef231939e9481f443ac3425e7f96b6
Theobl08 <theobl08@gmail.com>
2026-01-21 15:54:34 +0100

    Potion caudrons now emmits particle when filled

--- src/main/java/net/theobl/extension/block/PotionCauldronBlock.java
+++ src/main/java/net/theobl/extension/block/PotionCauldronBlock.java
@@ -2,11 +2,15 @@ package net.theobl.extension.block;
 
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Holder;
 import net.minecraft.core.cauldron.CauldronInteraction;
+import net.minecraft.core.particles.ColorParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.util.Util;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
@@ -16,6 +20,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.AbstractCauldronBlock;
@@ -87,6 +92,22 @@ public class PotionCauldronBlock extends AbstractCauldronBlock {
 //            }
 //        }
 //    }
+
+    @Override
+    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
+        super.animateTick(state, level, pos, random);
+        if (random.nextInt(10) % 5 == 0) {
+            Minecraft.getInstance().particleEngine.createParticle(
+                    ColorParticleOption.create(ParticleTypes.ENTITY_EFFECT, PotionContents.getColorOptional(potion.value().getEffects()).orElse(PotionContents.BASE_POTION_COLOR)),
+                    pos.getX() + 0.45 + random.nextDouble() * 0.2,
+                    pos.getY() + (double) state.getValue(LEVEL) / MAX_FILL_LEVEL,
+                    pos.getZ() + 0.45 + random.nextDouble() * 0.2,
+                    0.0,
+                    0.7,
+                    0.0
+            );
+        }
+    }
 
     public static void lowerFillLevel(BlockState state, Level level, BlockPos pos) {
         int newLevel = state.getValue(LEVEL) - 1;
